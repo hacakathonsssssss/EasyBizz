167a74785911d7d37df5e2daff74793c8ef43d92
Beetroot16 <vishrut.deshmukh@somaiya.edu>
2023-02-04 19:24:26 +0530

    added admin_signup

--- server/app.js
+++ server/app.js
@@ -66,5 +66,29 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
       });
     });
   });
-   app.listen(3000, () => console.log('Server started on port 3000'));
+
+  app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 10, (err, hash) => {
+      const collection = db.collection("users");
+      collection.findOne({ email: email }, { projection: { password: 1 }} ,function(err, result) {
+        bcrypt.compare(password,result.password,function(err,result){
+          if(err){
+            console.log(err);
+            return;
+          }
+          if(result){
+            console.log("match");
+            res.render('success');
+          }else{
+            console.log("dont match");
+            res.render('failure');
+          }
+        });
+      });
+    });
+  });
+  
+  app.listen(3000, () => console.log('Server started on port 3000'));
 });
